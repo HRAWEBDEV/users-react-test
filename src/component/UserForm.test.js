b0df06d7e301fe1a98bcd8b6852872b9730bc4c3
hamidwise1 <hrawebdev@gmail.com>
2023-09-06 12:33:17 +0430

    add submit user form test

--- src/component/UserForm.test.js
+++ src/component/UserForm.test.js
@@ -1,16 +1,43 @@
 import { render, screen } from '@testing-library/react';
 import user from '@testing-library/user-event';
-import UserForm from './UserForm';
 import '@testing-library/jest-dom';
+import UserForm from './UserForm';
 
-test('it shows two inputs and a button', () => {
- // * render the component
+test('it shows two inputs and one button', () => {
  render(<UserForm />);
- // * manipulate the component or find a component in it
  const inputs = screen.getAllByRole('textbox');
  const button = screen.getByRole('button');
- // * assertion - make sure the component is doing
- // * what we expect it to do
+
  expect(inputs).toHaveLength(2);
  expect(button).toBeInTheDocument();
+});
+
+test('submit calls onAddUser', () => {
+ // * NOT THE BEST IMPLEMENTATION
+ const users = [];
+ const firstname = 'hamid reza';
+ const email = 'hamidwise1@gmail';
+ const callBack = (newUser) => {
+  users.push(newUser);
+ };
+
+ // * render component
+ render(<UserForm onAddUser={callBack} />);
+ // * find inputs
+ const [firstnameInput, emailInput] = screen.getAllByRole('textbox');
+ // * simulate clicking input
+ user.click(firstnameInput);
+ // * simulate typing the firstname
+ user.keyboard(firstname);
+
+ user.click(emailInput);
+ user.keyboard(email);
+
+ // * find the submit button
+ const button = screen.getByRole('button');
+ user.click(button);
+ // * assertions (onAddUser is called)
+ expect(users).toHaveLength(1);
+ expect(users[0].firstname).toEqual(firstname);
+ expect(users[0].email).toEqual(email);
 });
