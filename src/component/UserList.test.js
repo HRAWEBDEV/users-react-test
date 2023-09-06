001c3004db1b18df3ff8d5fb1c8415d162625c38
hamidwise1 <hrawebdev@gmail.com>
2023-09-06 18:48:12 +0430

    dry test code

--- src/component/UserList.test.js
+++ src/component/UserList.test.js
@@ -2,20 +2,23 @@ import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from './UserList';
 
-const users = [{ firstname: 'hamid reza', email: 'hamidwise1@gmail.com' }];
-test('render one row per user', () => {
- // * render the component
+const renderComponent = () => {
+ const users = [{ firstname: 'hamid reza', email: 'hamidwise1@gmail.com' }];
  const { container } = render(<UserList users={users} />);
- // * query rows
- // const rows = within(screen.getByTestId('users')).getAllByRole('row');
+ render(<UserList users={users} />);
+
+ return { users, container };
+};
+
+test('render one row per user', () => {
+ const { container } = renderComponent();
  // eslint-disable-next-line
  const rows = container.querySelectorAll('tbody tr');
  expect(rows).toHaveLength(1);
 });
 
 test('render the email and name of each use', () => {
- render(<UserList users={users} />);
-
+ const { users } = renderComponent();
  users.forEach((user) => {
   const firstname = screen.getByRole('cell', { name: user.firstname });
   const email = screen.getByRole('cell', { name: user.email });
