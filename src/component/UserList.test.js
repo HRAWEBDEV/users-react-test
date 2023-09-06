3c17bcf8bf3ac811f8432e3da4ff7fe8827c4346
hamidwise1 <hrawebdev@gmail.com>
2023-09-06 18:42:36 +0430

    add user list render rows test

--- src/component/UserList.test.js
+++ src/component/UserList.test.js
@@ -2,8 +2,8 @@ import { render, screen, within } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import UserList from './UserList';
 
+const users = [{ firstname: 'hamid reza', email: 'hamidwise1@gmail.com' }];
 test('render one row per user', () => {
- const users = [{ firstname: 'hamid reza', email: 'hamidwise1@gmail.com' }];
  // * render the component
  const { container } = render(<UserList users={users} />);
  // * query rows
@@ -11,4 +11,16 @@ test('render one row per user', () => {
  // eslint-disable-next-line
  const rows = container.querySelectorAll('tbody tr');
  expect(rows).toHaveLength(1);
+});
+
+test('render the email and name of each use', () => {
+ render(<UserList users={users} />);
+
+ users.forEach((user) => {
+  const firstname = screen.getByRole('cell', { name: user.firstname });
+  const email = screen.getByRole('cell', { name: user.email });
+
+  expect(firstname).toBeInTheDocument();
+  expect(email).toBeInTheDocument();
+ });
 });
